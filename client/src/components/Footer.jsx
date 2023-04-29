688eb94d617e61cf52b408ac1e1c2a82e02251fa
decourtney <53956789+decourtney@users.noreply.github.com>
2023-04-29 00:46:03 -0500

    footer styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer flex-col bg-gray-800 text-white py-6 text-center justify-center">
+    // Replace mt-auto with fixed bottom-0 for Sticky footer
+    <footer className="footer flex-col mt-auto w-full bg-gray-800 text-white py-6 text-center justify-center">
       <p>&copy; 2023 RealityCheck. All rights reserved.</p>
       <nav>
         <ul className="flex justify-center mt-4">
-          <li><a href="/about" className="mx-3">About Us</a></li>
-          <li><a href="/contact" className="mx-3">Contact Us</a></li>
-          <li><a href="/privacy" className="mx-3">Privacy Policy</a></li>
-          <li><a href="/terms" className="mx-3">Terms of Service</a></li>
+          <li>
+            <a href="/about" className="mx-3">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className="mx-3">
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a href="/privacy" className="mx-3">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="/terms" className="mx-3">
+              Terms of Service
+            </a>
+          </li>
         </ul>
       </nav>
     </footer>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -34,51 +34,43 @@ const Login = () => {
     });
   };
 
-
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h2 className="text-3xl mb-4 font-bold">Login</h2>
-      <form className="w-full max-w-md" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={userFormData.email}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={userFormData.password}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign In
-          </button>
-        </div>
-      </form>
+    <div className="flex flex-col max-w-md mx-auto items-center bg-gray-800 p-5 shadow-md">
+      <div className="flex flex-col items-center">
+        <h2 className="text-3xl mb-4 font-bold">Login</h2>
+        <form className="w-full max-w-md" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="email"
+              name="email"
+              value={userFormData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              name="password"
+              value={userFormData.password}
+              onChange={handleInputChange}
+              placeholder="******************"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
