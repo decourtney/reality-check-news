719dda63ca6a6b0301a4fd2acd0ef8f409442427
decourtney <53956789+decourtney@users.noreply.github.com>
2023-09-20 22:00:12 -0500

    added cors for when running on the server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/realitycheckDB');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/realitycheckDB",
+  { useNewUrlParser: true }
+);
 
 module.exports = mongoose.connection;
